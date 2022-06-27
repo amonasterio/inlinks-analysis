fe5187c061425312e73d3fd11f46e2a9ef314ae1
Alejandro Monasterio <amonasterio@gmail.com>
2022-06-27 12:16:10 +0200

    añado aggrid

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+from turtle import up
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+
+st.set_page_config(
+   page_title="Análisis de enlaces",
+   layout="wide"
+)
 
 
+st.title("Análisis de enlaces")
+@st.cache
 def getInlinksPorURL(df):
     df_agrupado=df.groupby(['Destination','Follow','Status Code']).count()['Source'].reset_index(name="count")
     df_follow=df_agrupado[df_agrupado['Follow']==True]
@@ -20,32 +30,35 @@ if f_entrada is not None:
     df_filtrado=pd.read_csv(f_entrada)
     df_mask=(df_filtrado['Type']=='Hyperlink')&(df_filtrado['Link Position']=='Content')&(df_filtrado['Source']!=df_filtrado['Destination'])
     df_filtrado=df_filtrado[df_mask]
-    tipo_filtro_url = st.radio(
-     "Tipo de filtrado",
-     ('Contiene','Igual a'))
-    filtro_url=st.text_input("Introduzca URL que quiera filtrar")
-    if tipo_filtro_url=="Igual a":
-        df_mask_url=df_filtrado['Destination']==filtro_url
-    else:
-        df_mask_url=df_filtrado['Destination'].str.contains(filtro_url)
-    df_filtrado=df_filtrado[df_mask_url]
-    result=getInlinksPorURL(df_filtrado)
-    st.dataframe(result)
-    st.download_button(
-                label="Descargar como CSV",
-                data=result.to_csv(index = False).encode('utf-8'),
-                file_name='agrupado.csv',
-                mime='text/csv',
-            )
     
-    df = df_filtrado.drop(columns=["Type","Size (Bytes)","Status","Target","Path Type","Link Position"])
-    url=st.text_input(label="Analizar URL")
-    df_mask=df['Destination']==url
-    df=df[df_mask]
-    st.dataframe(df)
-    st.download_button(
-                label="Descargar como CSV",
-                data=df.to_csv(index = False).encode('utf-8'),
-                file_name='agrupado.csv',
-                mime='text/csv',
-            )
+    result=getInlinksPorURL(df_filtrado)
+    
+
+    gb=GridOptionsBuilder.from_dataframe(result)
+    gb.configure_pagination()
+    gb.configure_side_bar()
+    #sel_mode=st.radio('Selection type', options=['single','multiple'])   
+    gb.configure_selection(selection_mode='multiple',use_checkbox=True)
+    gb.configure_default_column(groupable=True, enableRowGroup=True, aggFunc="count")
+    gridoptions=gb.build()
+    st.subheader('Resumen de enlaces')
+    grid_table=AgGrid(result,height=600,gridOptions=gridoptions,update_mode=GridUpdateMode.SELECTION_CHANGED,enable_enterprise_modules=True)  
+    sel_rows=grid_table['selected_rows']
+    #st.write(sel_rows)
+    if len(sel_rows) > 0:
+        #Filtramos la URL seleccionada en el dataframe con todos los enlaces
+        filtro=[]
+        for i in sel_rows:
+            destino=i["Destination"]
+            filtro.append(destino)
+        df = df_filtrado.drop(columns=["Type","Size (Bytes)","Status","Target","Path Type","Link Position"])
+        boolean_series = df["Destination"].isin(filtro) 
+        df=df[boolean_series]
+        gb_enlaces=GridOptionsBuilder.from_dataframe(df)
+        gb_enlaces.configure_side_bar()
+        gb_enlaces.configure_default_column(groupable=True, enableRowGroup=True, aggFunc="count", editable=True)
+        gridoptions_enlaces=gb_enlaces.build()
+        st.subheader('Enlaces apuntando a destinos seleccionados')
+        grid_table_enlaces=AgGrid(df,gridOptions=gridoptions_enlaces,enable_enterprise_modules=True)
+
+ 
